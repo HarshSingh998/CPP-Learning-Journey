1b0dbefbd7835546650b9effc4bdd84817369840
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-12 14:51:33 +0530

    Class - 6

--- Class/Class - 6/Control Flow Statements.cpp
+++ Class/Class - 6/Control Flow Statements.cpp
@@ -1,36 +1,40 @@
 
-
-
-
-
-
-
 // Control Flow Statement 
 
-// Who Can Give Vote 
+#include <iostream>
+using namespace std;
 
-// int age = 19;
+int main () {
 
-// if ( age >= 18 ){
-//     cout << " Valid Voter" << endl;
-// }
-// else {
-//     cout << " Not A Valid Voter" << endl;
-// }
+
+
+
+// Who Can Give Vote  ->>>
+
+int age = 19;
+
+if ( age >= 18 ){
+    cout << " Valid Voter" << endl;
+}
+else {
+    cout << " Not A Valid Voter" << endl;
+}
+
+
 
 
 
 
 // In Two Number Which Is Greater 
 
-// int a = 7;
-// int b = 10;
-// if ( a > b ){
-//     cout << a << endl;
-// }
-// else {
-//     cout << b << endl;
-// }
+int a = 7;
+int b = 10;
+if ( a > b ){
+    cout << a << endl;
+}
+else {
+    cout << b << endl;
+}
 
 
 
@@ -38,150 +42,151 @@
 
 // else if ladder use in multiple condition and sab me se jo true ho whi chle baki na chle 
 
-// int a = 10 , b = 20 ;
+int a = 10 , b = 20 ;
 
-// if ( a <= 50 ){
-//     cout << "Line 1" << endl;
-// }
+if ( a <= 50 ){
+    cout << "Line 1" << endl;
+}
 
-// else if ( 10 > 7){
-//     cout << "Line 2" << endl;
-// }
+else if ( 10 > 7){
+    cout << "Line 2" << endl;
+}
 
-// else if ( 100 > 7){
-//     cout << "Line 3" << endl;
-// }
+else if ( 100 > 7){
+    cout << "Line 3" << endl;
+}
 
-// else if ( 10 > 17){
-//     cout << "Line 4" << endl;
-// }
+else if ( 10 > 17){
+    cout << "Line 4" << endl;
+}
 
-// else {
-//     cout << "Last else" << endl;
-// }
+else {
+    cout << "Last else" << endl;
+}
 
 
 
 
 
 
-// int a = 10 , b = 20 ;
 
-// if ( a > 50 ){
-//     cout << "Line 1" << endl;
-// }
 
-// else if ( 10 < 7){
-//     cout << "Line 2" << endl;
-// }
+int a = 10 , b = 20 ;
 
-// else if ( 100 > 7){
-//     cout << "Line 3" << endl;
-// }
+if ( a > 50 ){
+    cout << "Line 1" << endl;
+}
 
-// else if ( 10 > 17){
-//     cout << "Line 4" << endl;
-// }
+else if ( 10 < 7){
+    cout << "Line 2" << endl;
+}
 
-// else {
-//     cout << "Last else" << endl;
-// }
+else if ( 100 > 7){
+    cout << "Line 3" << endl;
+}
 
+else if ( 10 > 17){
+    cout << "Line 4" << endl;
+}
 
+else {
+    cout << "Last else" << endl;
+}
 
 
 
 
-// int a = 10 , b = 20 ;
 
-// if ( a > 50 ){
-//     cout << "Line 1" << endl;
-// }
 
-// else if ( 10 < 7){
-//     cout << "Line 2" << endl;
-// }
 
-// else if ( 100 < 7){
-//     cout << "Line 3" << endl;
-// }
 
-// else if ( 10 > 17){
-//     cout << "Line 4" << endl;
-// }
+int a = 10 , b = 20 ;
 
-// else {
-//     cout << "Last else" << endl;
-// }
+if ( a > 50 ){
+    cout << "Line 1" << endl;
+}
 
+else if ( 10 < 7){
+    cout << "Line 2" << endl;
+}
 
+else if ( 100 < 7){
+    cout << "Line 3" << endl;
+}
 
+else if ( 10 > 17){
+    cout << "Line 4" << endl;
+}
 
+else {
+    cout << "Last else" << endl;
+}
 
-// int a = 10 , b = 20 ;
 
-// if ( a < 50 ){
-//     cout << "Line 1" << endl;
-// }
 
-// if ( 10 > 7){
-//     cout << "Line 2" << endl;
-// }
 
-// if ( 100 > 7){
-//     cout << "Line 3" << endl;
-// }
 
-// if ( 10 < 17){
-//     cout << "Line 4" << endl;
-// }
 
-// else {
-//     cout << "Last else" << endl;
-// }
 
 
+int a = 10 , b = 20 ;
 
+if ( a < 50 ){
+    cout << "Line 1" << endl;
+}
 
+if ( 10 > 7){
+    cout << "Line 2" << endl;
+}
 
-// int a = 10 , b = 20 ;
+if ( 100 > 7){
+    cout << "Line 3" << endl;
+}
 
-// if ( a < 50 ){
-//     cout << "Line 1" << endl;
-// }
+if ( 10 < 17){
+    cout << "Line 4" << endl;
+}
 
-// if ( 10 > 7){
-//     cout << "Line 2" << endl;
-// }
+else {
+    cout << "Last else" << endl;
+}
 
-// if ( 100 > 7){
-//     cout << "Line 3" << endl;
-// }
 
-// if ( 10 > 17){
-//     cout << "Line 4" << endl;
-// }
 
-// else {
-//     cout << "Last else" << endl;
-// }
 
 
+int a = 10 , b = 20 ;
 
+if ( a < 50 ){
+    cout << "Line 1" << endl;
+}
 
+if ( 10 > 7){
+    cout << "Line 2" << endl;
+}
 
-// Difference b/w IF Ladder And Else Ladder
-// if Is independent but else if is dependent 
+if ( 100 > 7){
+    cout << "Line 3" << endl;
+}
 
+if ( 10 > 17){
+    cout << "Line 4" << endl;
+}
 
+else {
+    cout << "Last else" << endl;
+}
 
 
 
 
 
+// Difference b/w IF Ladder And Else Ladder -> if Is independent but else if is dependent 
 
 
 
 
 
 
+    return 0;
+}
