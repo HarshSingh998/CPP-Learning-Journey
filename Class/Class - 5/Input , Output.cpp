650b414b365364e923d7061919228f9b2d8e7d49
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-12 14:48:06 +0530

    Class - 5

--- Class/Class - 5/Input , Output.cpp
+++ Class/Class - 5/Input , Output.cpp
@@ -1,22 +1,25 @@
+
 // Input Or Output
 
-// int val;
-// cin >> val;
-// cout << "Value = " << val << endl;
+int val;
+cin >> val;
+cout << "Value = " << val << endl;
 
 
 
-// int val ;
-// cout << "Enter a Number : " ;
-// cin >> val;
-// cout << "Value = " << val << endl;
+int val ;
+cout << "Enter a Number : " ;
+cin >> val;
+cout << "Value = " << val << endl;
 
 
 
-// int a , b ;
-// cout << "Enter Two Number : " ;
-// cin >> a >> b;
-// cout << "Sum = " << (a + b) << endl;
+int a , b ;
+cout << "Enter Two Number : " ;
+cin >> a >> b;
+cout << "Sum = " << (a + b) << endl;
+
+
 
 
 
@@ -30,36 +33,20 @@ using namespace std;
 
 int main() {
 
-// string name;
-// int age;
+string name;
+int age;
 
-// cout << " Enter Your Name = " ;
-// cin >> name;   
+cout << " Enter Your Name = " ;
+cin >> name;   
 
 /* cin kehta hai agr space ya next line character dikhta h to input lena bnd kr dega isliye hm 
 string me getline ka use krte h */
 
 
-// cout << " Enter Your Age = " ;
-// cin >> age;
+cout << " Enter Your Age = " ;
+cin >> age;
 
-// cout << " Hello " << name << ", You Are " << age << " Years Old." << endl;
-
-
-
-
-
-
-// string name;
-// int age;
-
-// cout << " Enter Your Name = " ;
-// getline(cin, name);
-
-// cout << " Enter Your Age = " ;
-// cin >> age;
-
-// cout << " Hello " << name << ", You Are " << age << " Years Old." << endl;
+cout << " Hello " << name << ", You Are " << age << " Years Old." << endl;
 
 
 
@@ -68,18 +55,16 @@ string me getline ka use krte h */
 
 
 
-// string name;
-// int age;
+string name;
+int age;
 
-// cout << " Enter Your Age = " ;
-// cin >> age;
+cout << " Enter Your Name = " ;
+getline(cin, name);
 
-// cout << " Enter Your Name = " ;
-// getline(cin, name);
-// // getline kehta h ki space ko ignore krega but /n ( Next line Character )ko nhi 
+cout << " Enter Your Age = " ;
+cin >> age;
 
-// cout << " Hello " << name << ", You Are " << age << " Years Old." << endl;
-
+cout << " Hello " << name << ", You Are " << age << " Years Old." << endl;
 
 
 
@@ -87,36 +72,19 @@ string me getline ka use krte h */
 
 
 
-// string name;
-// string name1;
-// int age;
-
-// cout << " Enter Your Age = " ;
-// cin >> age;
-
-// cout << " Enter Your Name = " ;
-// getline(cin, name1);
-// getline(cin, name);
-
-// cout << " Hello " << name << ", You Are " << age << " Years Old." << endl;
 
 
+string name;
+int age;
 
+cout << " Enter Your Age = " ;
+cin >> age;
 
+cout << " Enter Your Name = " ;
+getline(cin, name);
+// getline kehta h ki space ko ignore krega but /n ( Next line Character )ko nhi 
 
-
-
-// string name;
-// int age;
-
-// cout << " Enter Your Age = " ;
-// cin >> age;
-
-// cout << " Enter Your Name = " ;
-// cin.ignore();
-// getline(cin, name);
-
-// cout << " Hello " << name << ", You Are " << age << " Years Old." << endl;
+cout << " Hello " << name << ", You Are " << age << " Years Old." << endl;
 
 
 
@@ -124,11 +92,49 @@ string me getline ka use krte h */
 
 
 
-// int a = 10 , b = 20;
-// cout << a << b << "\n";
-// cout << a << " " << b << "\n";
-// cout << a << "\n" << b << "\n";
-// cout << a << endl << b << "\n";
+
+string name;
+string name1;
+int age;
+
+cout << " Enter Your Age = " ;
+cin >> age;
+
+cout << " Enter Your Name = " ;
+getline(cin, name1);
+getline(cin, name);
+
+cout << " Hello " << name << ", You Are " << age << " Years Old." << endl;
+
+
+
+
+
+
+
+string name;
+int age;
+
+cout << " Enter Your Age = " ;
+cin >> age;
+
+cout << " Enter Your Name = " ;
+cin.ignore();
+getline(cin, name);
+
+cout << " Hello " << name << ", You Are " << age << " Years Old." << endl;
+
+
+
+
+
+
+
+int a = 10 , b = 20;
+cout << a << b << "\n";
+cout << a << " " << b << "\n";
+cout << a << "\n" << b << "\n";
+cout << a << endl << b << "\n";
 
 
 // ( endl ) - Slow because ye kisi bhi kaam ko 2 process me krta hai pehle data leta ha or fir uska khali krta h 
@@ -138,25 +144,29 @@ string me getline ka use krte h */
 
 
 
+
+
+
+
 // Escape Sequence 
 
-// string name = "Harsh";
-// cout << "Hello, " " << name << """ << "\n";
+string name = "Harsh";
+cout << "Hello, " " << name << """ << "\n";
 
 
 
-// string name = "Harsh";
-// cout << "Hello, \"" << name << "\"" << "\n";
+string name = "Harsh";
+cout << "Hello, \"" << name << "\"" << "\n";
 
 
 
-// string name = "Harsh";
-// cout << "Hello, \'" << name << "\'" << "\n";
+string name = "Harsh";
+cout << "Hello, \'" << name << "\'" << "\n";
 
 
 
-// string name = "Harsh";
-// cout << "Hello, \t" << name << "\n";
+string name = "Harsh";
+cout << "Hello, \t" << name << "\n";
 
 
 
