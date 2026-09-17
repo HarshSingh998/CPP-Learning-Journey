ff977cbe1df8f712f7001613ff860c0146a8c5d7
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-17 09:59:30 +0530

    College

---
+++ College Practice/2_while_loop.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter A Number :-  ";
+    cin >> n;
+    int i = 1;
+    while (i<=n){
+        cout << "1" << endl;
+        i++;
+    }
+
+
+
+
+
+
+// Counting A Number
+
+    int n ;
+    cout << "Enter A Number :-  ";
+    cin >> n;
+    int i = 1;
+    while (i<=n){
+        cout << i << endl;
+        i++;
+    }
+
+
+
+    return 0;
+}
