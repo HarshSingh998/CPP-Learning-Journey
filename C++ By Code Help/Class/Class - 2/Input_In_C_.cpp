cbf3dd0ca426ab54a56cd0ff7cc0a5537641c4ac
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-18 23:01:16 +0530

    Class - 2

---
+++ C++ By Code Help/Class/Class - 2/Input_In_C_.cpp
@@ -0,0 +1,74 @@
+
+// cout - print krwane ka work krta tha 
+// cin - jo ki terminal windows se input lene ka kaam krta hai
+
+// ( cout prints on that output windows & cin takes input from that windows )
+
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    // Variable Declare
+    int age;
+    cout << "Enter Your Age :- " << endl;
+
+    // Input le rhe hai yha pe
+    cin >> age;
+    cout << "Your Age is :- " << age << endl;
+
+
+
+
+
+    int marks;
+    cout << "Enter Your Marks :- " << endl;
+
+    cin >> marks;
+    cout << "Your Marks is :- " << marks << endl;
+
+
+
+
+
+    float marks;
+    cout << "Enter Your Marks :- " << endl;
+
+    cin >> marks;
+    cout << "Your Marks is :- " << marks << endl;
+
+
+
+
+
+    double marks;
+    cout << "Enter Your Marks :- " << endl;
+
+    cin >> marks;
+    cout << "Your Marks is :- " << marks << endl;
+
+
+
+
+    char marks;
+    cout << "Enter Your Marks :- " << endl;
+
+    cin >> marks;
+    cout << "Your Marks is :- " << marks << endl;
+
+
+
+
+    bool marks;
+    cout << "Enter Your Marks :- " << endl;
+
+    cin >> marks;
+    cout << "Your Marks is :- " << marks << endl;
+
+// jab aap Bool types ka variable bnate ho to usko input lene ki koshish krte ho to input aap kbhi bhi true or false karke nhi likh skte ho aapko 0 or 1 ka use krna padega. 
+
+
+    return 0;
+}
+
