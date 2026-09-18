096af4e4a2a298cc3c2e148309bec46c7bf298ad
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-18 08:56:26 +0530

    Code Help

---
+++ C++ By Code Help/Class/Class - 1/Variables_Datatypes.cpp
@@ -0,0 +1,100 @@
+
+// Data ko store karne ke liye variables ka use kiya jata hai (variable ka matlab hai data ko store karne ke liye ek container) aur variable ko define karne ke liye hume uska datatype specify karna padta hai (datatype ka matlab hai ki variable me kis type ka data store hoga)
+
+
+// jab bhi me kisi memory block ko name deta hu to wo name variable kehlata hai
+
+// int marks = 50; // int ka matlab hai integer wo ek data type hai (integer ka matlab hai whole number) aur marks variable me 50 store ho gaya (50 ek integer value hai)
+// aapne ek memory block ke andr aapne 50 ko store kar diya aur us memory block ko aapne marks naam de diya (marks ek variable hai jisme 50 store ho gaya) aur iske liye aapne int datatype ka use kiya (int ka matlab hai integer aur ye ek datatype hai)
+
+// int age = 25;
+
+// Variables - Named Storage Memory Locations ( Jis memory block ke andr aap data ko store krne wale hai uske naam ko aapne variable kediya hai.)
+
+// Datatypes Variable = Value;
+
+// Datatypes - The Datatypes specifies the size and type of information the variable will store. 
+
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     // int age = 18;
+//     // cout << "My Age Is " << age << endl;
+
+
+//     // Declaration
+//     int age;
+//     cout << age;
+
+//     // Definition
+//     int age = 18;
+//     cout << age << endl;
+
+// // same naam ke variable ko aap ek hi programme me do baar define nhi kr skte (same naam ke variable ko aap ek hi programme me do baar define nhi kr skte)
+
+
+//     // Manipulation
+//     age = 20;
+//     cout << age << endl;
+
+
+
+//     return 0;
+// }
+
+
+
+
+// Declaration ka mtb hai ki apne variable ko create kr diya but usme koi value assign nhi ki (Declaration ka matlab hai ki apne variable ko create kr diya hai but usme koi value assign nhi ki) 
+// Initialization ( Definition ) ka mtb hai ki apne variable ko create kr diya aur usme value assign bhi krdi 
+// Initialization ka matlab hai ki apne variable ko create kr diya aur usme value assign bhi krdi)
+// Manipulation Or Updation Also called as Re-Initialization ( Manipulation ka matlab hai ki apne variable me jo value store hai usko change krna ya update krna)
+
+
+
+
+
+// C++ Datatypes - Primitive Datatypes or Built-in Datatypes ( Primitive Datatypes - int, float, double, char, bool )
+//                 Derived Datatypes
+//                 User-defined Datatypes
+                
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // int
+    int count = 5;
+
+    // float
+    float share = 3.14;
+
+    // char
+    char alphabet = 'z'
+
+    // double
+    double weight = 55.68891;
+
+    // boolean
+    bool isMale = true;
+    bool isMale = 1;
+    bool isFemale = false;
+    bool isFemale = 0;
+// In boolean 1 is True And 0 is False
+
+
+    return 0;
+}
+
+
+
+
