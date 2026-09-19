9edfa249d91de1b47e6d4e72354ae5998d29681a
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-19 19:15:57 +0530

    Class - 4

--- C++ By Code Help/Class/Class - 4/4_While_Loop_.cpp
+++ C++ By Code Help/Class/Class - 4/4_While_Loop_.cpp
@@ -3,9 +3,12 @@ using namespace std;
 
 int main() {
 
+    // Initialization
     int i = 1;
+    // While ( Condition )
     while (i <= 10) {
         cout << i << endl;
+    // Updation
         i++;
     }
 
--- C++ By Code Help/Class/Class - 4/5_Do_While_Loop_.cpp
+++ C++ By Code Help/Class/Class - 4/5_Do_While_Loop_.cpp
@@ -1,6 +1,24 @@
 
 
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    // Initialization
+    int i = 1;
+    do {
+        // Logic
+        cout << i << endl;
+        // Updation
+        i++;
+    } while (i <= 10);
+
+
+
+
+    return 0;
+}
 
 
 
