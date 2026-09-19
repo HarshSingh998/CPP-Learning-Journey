a9ba728fa193f4f5974398dc275bd5069e66def9
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-19 19:56:05 +0530

    Class - 4

--- C++ By Code Help/Class/Class - 4/5_Do_While_Loop_.cpp
+++ C++ By Code Help/Class/Class - 4/5_Do_While_Loop_.cpp
@@ -12,40 +12,23 @@ int main() {
         cout << i << endl;
         // Updation
         i++;
+    // Condition
     } while (i <= 10);
 
 
 
 
+
+
+
+    int count = 20;
+    int i = 1;
+    do{
+        cout << "Harsh" << " ";
+        i = i + 1;
+    } while (i <= count);
+    cout << endl;
+
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
