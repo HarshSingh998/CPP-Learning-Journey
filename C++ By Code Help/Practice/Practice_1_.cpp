69c513599047949a3c0314d377fea6d642e265e3
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-20 10:12:15 +0530

    Practice - 1

--- C++ By Code Help/Practice/Practice_1_.cpp
+++ C++ By Code Help/Practice/Practice_1_.cpp
@@ -6,48 +6,50 @@ using namespace std;
 
 int main() {
 
+
+    // Counting From 1 To 100
     for(int i = 1 ; i <= 100 ; i++){
         cout << i << endl;
     }
 
 
 
-
+    // Counting From 100 To 1
     for(int i = 100; i >= 1; i--){
         cout << i << endl;
     }
 
 
 
-
+    // Counting From 0 To -10
     for(int i = 0; i >= -10; i--){
         cout << i << endl;
     }
 
 
 
-
+    // Print Your Name 50 Time
     for(int i = 1; i <= 50; i++){
         cout << i << " -> " << "Harsh Singh" << endl;
     }
 
 
 
-
+    // Print A Table Of 7
     for(int i = 1; i <= 10; i++){
         cout << 7*i << endl;
     }
 
 
 
-
+    // Print Alphabets In Uppercase
     for(char i = 'A'; i <= 'Z'; i++){
         cout << i << endl;
     }
 
 
 
-
+    // Print Alphabets In Lowercase
     for(char i = 'a'; i <= 'z'; i++){
         cout << i << endl;
     }
@@ -55,5 +57,3 @@ int main() {
 
     return 0;
 }
-
-
