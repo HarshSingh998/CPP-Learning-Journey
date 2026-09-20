74887da9f466cd4a79c14b0dd6a4245ec2ee0ccd
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-20 10:09:24 +0530

    Practice - 1

---
+++ C++ By Code Help/Practice/Practice_1_.cpp
@@ -0,0 +1,59 @@
+
+// Class - 6
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for(int i = 1 ; i <= 100 ; i++){
+        cout << i << endl;
+    }
+
+
+
+
+    for(int i = 100; i >= 1; i--){
+        cout << i << endl;
+    }
+
+
+
+
+    for(int i = 0; i >= -10; i--){
+        cout << i << endl;
+    }
+
+
+
+
+    for(int i = 1; i <= 50; i++){
+        cout << i << " -> " << "Harsh Singh" << endl;
+    }
+
+
+
+
+    for(int i = 1; i <= 10; i++){
+        cout << 7*i << endl;
+    }
+
+
+
+
+    for(char i = 'A'; i <= 'Z'; i++){
+        cout << i << endl;
+    }
+
+
+
+
+    for(char i = 'a'; i <= 'z'; i++){
+        cout << i << endl;
+    }
+
+
+    return 0;
+}
+
+
