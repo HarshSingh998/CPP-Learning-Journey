d1b7369b54a2bb832538a8c365b6b2848d4d28c4
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-20 14:20:45 +0530

    Code Help Practice - 2

---
+++ C++ By Code Help/Practice/Code_Help_2_.cpp
@@ -0,0 +1,55 @@
+
+// Code Help Question From 201 To 210. 
+
+
+// Voting Eligibility Checker
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int age;
+
+    cout << "Enter Your Age :- " << endl;
+    cin >> age;
+
+    if(age >= 18){
+        cout << "Eligible To Vote" << endl;
+    }
+    else{
+        cout << "Not Eligible To Vote" << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+// Check Number Sign
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
