078f2acd9149f0ea0175deb5a06324f63011ee7a
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-21 06:56:53 +0530

    Class - 5

---
+++ C++ By Code Help/Class/Class - 5/1_Operators_.cpp
@@ -0,0 +1,150 @@
+
+// Operator Divide into 2 parts -> Unary & Binary
+// Unary jisme 1 hi operant lagta h ( -5 )
+// Binary me 2 operant lagta h ( 5 - 5 ) ( 9 * 2 )
+
+
+// Unary Operators ->> ( ++ ) & ( -- )
+
+// (++a)  ->>  Pre-increment ( pehle value ko increment karta hai aur fir use karta hai )
+// (a++)  ->>  Post-increment ( pehle value ko use karta hai aur fir increment karta hai )
+// (--a)  ->>  Pre-decrement ( pehle value ko decrement karta hai aur fir use karta hai )
+// (a--)  ->>  Post-decrement ( pehle value ko use karta hai aur fir decrement karta hai )
+
+
+
+
+
+
+
+// Arithmetic Operators ->> ( + ) , ( - ) , ( * ) , ( / ) , ( % )
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     int a = 10;
+//     int b = 5;
+
+//     cout << (a+b) << endl; // 15
+//     cout << (a-b) << endl; // 5
+//     cout << (a*b) << endl; // 50
+//     cout << (a/b) << endl; // 2
+//     cout << (a%b) << endl; // 0
+
+
+//     cout << (5/2) << endl; 
+// // Iska output 2 aaya balki 2.5 ana chahiye tha, kyuki 5 aur 2 dono hi integer hai aur integer division me decimal part ko ignore kar diya jata hai.
+// // Same agr hm float or double ko jb integer ke sath divide karenge to decimal part ko consider kiya jayega.
+
+//     cout << (5.0/2) << endl; // 2.5
+//     cout << (5*1.0/2) << endl; // 2.5
+
+
+//     return 0;
+// }
+
+
+
+
+// Relational Operators ->> ( == ) , ( != ) , ( > ) , ( < ) , ( >= ) , ( <= )
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     int a = 10;
+//     int b = 5;
+
+//     cout << (10 > 5) << endl;
+//     cout << (10 < 5) << endl;
+//     cout << (10 >= 5) << endl;
+//     cout << (10 <= 5) << endl;
+//     cout << (10 == 5) << endl;
+//     cout << (10 != 5) << endl;
+
+
+
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+// Logical Operartors ->> ( && ) , ( || ) , ( ! )  ->> logical operators are used to combine or modify multiple conditions.
+
+// ( && ) And Operators  ->>> If All Conditions Are True = True But If Anyone Is False = False
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     bool cond1 = true;
+//     bool cond2 = true;
+//     bool cond3 = false;
+
+//     if(cond1 && cond2 && cond3) {
+//         cout << "All Conditions Are True" << endl;
+//     }
+//     else{
+//         cout << "All Conditons Are Not True" << endl;
+//     }
+
+//     return 0;
+// }
+
+
+//  ( || ) OR Operators  ->>> If ALL Conditions Are False = False But If any Condition Are True = True
+
+// ( ! ) Not Operators  ->> If Condition Is True It Convert Into False And Vise-Versa. 
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     bool cond1 = true;
+//     cout << !cond1 << endl;
+
+//     bool cond2 = false;
+//     cout << !cond2 << endl;
+   
+
+//     return 0;
+// }
+
+
+
+
+// Assignment Operators  ->> ( = ) , ( += ) , ( -= ) , ( *= ) , ( /= ) , ( %= )
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
